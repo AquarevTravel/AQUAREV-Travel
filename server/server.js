f810f3bf18c9e410d6a5eaa7be20cee7374427ab
AquarevTravel <sdbka34@gmail.com>
2026-07-29 21:26:09 +0200

    Add billetterie route

--- server/server.js
+++ server/server.js
@@ -97,6 +97,7 @@ message:"Erreur serveur"
 });
 }
 });
+app.use("/billetterie",express.static(path.join(__dirname,"../billetterie")));
 app.use(express.static(path.join(__dirname,"..")));
 const PORT=process.env.PORT||3000;
 app.listen(PORT,"0.0.0.0",()=>{
